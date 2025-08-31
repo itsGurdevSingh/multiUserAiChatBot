9dd1796e298b842a8eb629a5c29df8d160f31ca5
gurdevsinghvip@gmail.com <gurdevsinghvip@gmail.com>
2025-08-31 19:56:31 +0530

    fixes

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,7 +28,7 @@ app.use('/chat', chatRouter);
 // Serve React build
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('*', (req, res) => {
+app.get('*name', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
